cd3291e460e8bd2f6f75622877f98fa90f804356
phev8 <phev8@users.noreply.github.com>
2021-10-19 16:35:30 +0200

    - Update libraries - add example placeholder

--- src/configs/pages/homePage.ts
+++ src/configs/pages/homePage.ts
@@ -2,10 +2,42 @@ import { PageConfig } from "case-web-app-core/build/types/pagesConfig"
 
 export const homePage = (path: string): PageConfig => {
     return {
-        path: '/home',
+        path: path,
         pageKey: 'home',
         rows: [
-
+            {
+                key: 'row1',
+                className: 'py-4',
+                columns: [
+                    {
+                        key: 'col1',
+                        className: 'col-12 col-md-4',
+                        items: [
+                            {
+                                itemKey: '1',
+                                config: {
+                                    type: 'placeholder',
+                                    label: 'content 1'
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        key: 'col2',
+                        className: 'col-12 col-md-4',
+                        items: [
+                            {
+                                className: 'mt-2 mt-md-0',
+                                itemKey: '2',
+                                config: {
+                                    type: 'placeholder',
+                                    label: 'content 2'
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
         ]
     }
 }
