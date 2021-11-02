6193301c4125f926638cc47225dfbc6652f64d0f
AnMu <92784573+AnMu27011@users.noreply.github.com>
2021-11-02 08:24:40 +0100

    layout of homepage with placeholders

--- src/configs/header.ts
+++ src/configs/header.ts
@@ -7,14 +7,14 @@ export const headerConfig: HeaderConfig = {
     image: {
       altKey: 'logoAlt',
       sm: {
-        url: '/images/placeholder_image.png',
+        url: '/images/simplified_logo_tekenradar.png',
         className: 'pb-2 d-block mx-auto',
-        height: 55
+        height: 100
       },
       lg: {
-        url: '/images/placeholder_image.png',
+        url: '/images/simplified_logo_tekenradar.png',
         className: 'pb-2',
-        height: 75
+        height: 120
       }
     },
     useLanguageSelector: false
--- src/configs/pages/homePage.ts
+++ src/configs/pages/homePage.ts
@@ -1,43 +1,160 @@
 import { PageConfig } from "case-web-app-core/build/types/pagesConfig"
 
 export const homePage = (path: string): PageConfig => {
-    return {
-        path: path,
-        pageKey: 'home',
-        rows: [
-            {
-                key: 'row1',
-                className: 'py-4',
-                columns: [
-                    {
-                        key: 'col1',
-                        className: 'col-12 col-md-4',
-                        items: [
-                            {
-                                itemKey: '1',
-                                config: {
-                                    type: 'placeholder',
-                                    label: 'content 1'
-                                }
-                            }
-                        ]
-                    },
-                    {
-                        key: 'col2',
-                        className: 'col-12 col-md-4',
-                        items: [
-                            {
-                                className: 'mt-2 mt-md-0',
-                                itemKey: '2',
-                                config: {
-                                    type: 'placeholder',
-                                    label: 'content 2'
-                                }
-                            }
-                        ]
-                    }
-                ]
-            }
+  return {
+    path: path,
+    pageKey: 'home',
+    rows: [
+      {
+        key: 'row1',
+        className: 'py-4',
+        columns: [
+          {
+            key: 'col1_1',
+            className: 'col-xs-12 col-md-8 col-md-offset-2',
+            items: [
+              {
+                itemKey: '1',
+                config: {
+                  type: 'placeholder',
+                  label: 'map',
+                  height: 600
+                }
+              }
+            ]
+          },
+          {
+            key: 'col1_2',
+            className: 'col-12 col-md-4',
+            items: [
+              {
+                itemKey: '2',
+                config: {
+                  type: 'placeholder',
+                  label: 'Tekenbeet melden',
+                  height: 600
+                }
+              }
+            ]
+          }
         ]
-    }
+      },
+      {
+        key: 'row2',
+        className: 'py-4',
+        columns: [
+          {
+            key: 'col2_1',
+            className: 'col',
+            items: [
+              {
+                itemKey: '3',
+                config: {
+                  type: 'placeholder',
+                  label: 'Informatie over Teken',
+                  height: 600
+                }
+              }
+            ]
+          },
+          {
+            key: 'col2_1',
+            className: 'col',
+            items: [
+              {
+                itemKey: '4',
+                config: {
+                  type: 'placeholder',
+                  label: 'Informatie over Ziekte van Lyme',
+                  height: 600
+                }
+              }
+            ]
+          },
+          {
+            key: 'col2_1',
+            className: 'col',
+            items: [
+              {
+                itemKey: '5',
+                config: {
+                  type: 'placeholder',
+                  label: 'Informatie over Tekenradar.nl',
+                  height: 600
+                }
+              }
+            ]
+          }
+        ]
+      },
+      {
+        key: 'row3',
+        className: 'py-4',
+        columns: [
+          {
+            key: 'col3_1',
+            className: 'col',
+            items: [
+              {
+                itemKey: '6',
+                config: {
+                  type: 'placeholder',
+                  label: 'Latest Niuews',
+                  height: 600
+                }
+              }
+            ]
+          }
+        ]
+      },
+      {
+        key: 'row4',
+        className: 'py-4',
+        columns: [
+          {
+            key: 'col4_1',
+            className: 'col',
+            items: [
+              {
+                itemKey: '7',
+                config: {
+                  type: 'placeholder',
+                  label: 'nou registreren',
+                  height: 600
+                }
+              }
+            ]
+          },
+          {
+            key: 'col4_2',
+            className: 'col',
+            items: [
+              {
+                itemKey: '8',
+                config: {
+                  type: 'placeholder',
+                  label: 'Veelgestelde vragen',
+                  height: 600
+                }
+              }
+            ]
+          },
+          {
+            key: 'col4_3',
+            className: 'col',
+            items: [
+              {
+                itemKey: '9',
+                config: {
+                  type: 'placeholder',
+                  label: 'Nieuwsbrief',
+                  height: 600
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
 }
