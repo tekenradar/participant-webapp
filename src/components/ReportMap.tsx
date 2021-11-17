cfcdabae11af90b72c6e29986c6d972af70dd6f6
phev8 <phev8@users.noreply.github.com>
2021-11-17 10:26:15 +0100

    some more placeholders for the map

--- src/components/ReportMap.tsx
+++ src/components/ReportMap.tsx
@@ -16,20 +16,41 @@ interface ReportMapProps {
 const ReportMap: React.FC<ReportMapProps> = (props) => {
   const { t, i18n } = useTranslation([props.pageKey]);
 
+  const mapTypes = ['Tekenmeldingen', 'Tekenverwachting']
 
+  /*
+  <h1 className="fs-1 text-center text-primary text-uppercase m-0 p-2">{props.label}</h1>
+      <p>{t(`${props.itemKey}`)}</p>
+      */
   return (
     <div
       className={clsx(
         // "d-flex justify-content-center align-items-center bg-grey-4"
       )}
     >
-      <h1 className="fs-1 text-center text-primary text-uppercase m-0 p-2">{props.label}</h1>
-      <p>{t(`${props.itemKey}`)}</p>
+      <nav aria-label="plot selection">
+        <ul className="pagination justify-content-center d-block d-sm-flex">
+          {mapTypes.map(dt =>
+            <li key={dt}
+              role="button"
+              className={clsx(
+                "page-item",
+                { 'active': dt === 'Tekenmeldingen' })}
+            //onClick={() => setSelectedSeries(dt)}
+            >
+              <span className="page-link">{dt}</span>
+            </li>
+          )}
+        </ul>
+      </nav>
+      <h3>Tekenmeldungen</h3>
+      <p>Overzicht van de meldingen van tekenbeten en erythema migrans(?) in de afgelopen vier weken.</p>
+
       <div>
         <MapContainer
           className="leaflet"
           style={{
-            height: 230
+            height: 450
           }}
           center={[52.2129919, 5.2793703]}
           zoom={2}
