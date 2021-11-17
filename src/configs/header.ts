e7ce162396c5cc09cd042b3e9ef9728375f5388a
AnMu <92784573+AnMu27011@users.noreply.github.com>
2021-11-17 15:10:55 +0100

    fix padding in markdown doc

--- src/configs/header.ts
+++ src/configs/header.ts
@@ -3,16 +3,16 @@ import { HeaderConfig } from "case-web-app-core/build/types/headerConfig";
 export const headerConfig: HeaderConfig = {
   config: {
     type: 'simpleLogo',
-    className: 'd-flex justify-content-center',
+    className: 'd-flex align-left',
     image: {
       altKey: 'logoAlt',
       sm: {
-        url: '/images/logo_tekenradar.png',
+        url: '/images/tekenradar-logoA3.png',
         className: 'pb-2 d-block mx-auto mt-1',
         height: 110
       },
       lg: {
-        url: '/images/logo_tekenradar.png',
+        url: '/images/tekenradar-logoA3.png',
         className: 'pb-2',
         height: 130
       }
