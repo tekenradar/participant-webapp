7acbf30467568146d6f25f7043cedfd8d21ac281
phev8 <phev8@users.noreply.github.com>
2022-02-15 11:42:14 +0100

    !!!yarn install needed - clickable header image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,24 @@
+import { InternalNavigator } from 'case-web-app-core';
 import { getExternalOrLocalContentURL } from 'case-web-ui';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header: React.FC = () => {
+  const [navigateTo, setNavigateTo] = useState<undefined | { url: string, replace: boolean }>();
+
+
   return (
     <header className="bg-secondary">
+      <InternalNavigator
+        navigateTo={navigateTo}
+      />
       <div className='container'>
         <div className='d-flex p-2 pt-0'>
           <img
+            onClick={() => setNavigateTo({ url: '/home', replace: true })}
             alt='Tekenradar Logo'
             src={getExternalOrLocalContentURL('/images/tekenradar-logo-cut.png')}
             height={75}
+            className='cursor-pointer'
           />
         </div>
       </div>
