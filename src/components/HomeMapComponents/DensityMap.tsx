348376c30f961d6824393613c9b9ab73a8878d53
phev8 <phev8@users.noreply.github.com>
2022-03-09 21:39:26 +0100

    load tb report map data from JSON

--- src/components/HomeMapComponents/DensityMap.tsx
+++ src/components/HomeMapComponents/DensityMap.tsx
@@ -8,15 +8,15 @@ const DensityMap: React.FC<DensityMapProps> = (props) => {
   return (
     <div className='p-2'>
       <div className='row'>
-        <div className='col-12 col-md-7'>
+        <div className='col-12 col-md-6'>
           <img
             width="100%"
             src={getExternalOrLocalContentURL('images/dichtheidkaart.png')}
             alt='Tekenbeetdichtheid'
           />
         </div>
-        <div className='col-12 col-md-5'>
-          <h3>Tekenbeetdichtheid</h3>
+        <div className='col-12 col-md-6'>
+          <h3 className='mt-2 mt-md-0'>Tekenbeetdichtheid</h3>
           <p>Op deze kaart is te zien waar de meeste tekenbeten via Tekenradar gemeld worden. In deze gebieden zitten niet alleen veel teken, maar komen ook veel mensen.</p>
         </div>
       </div>
