768f75129907a81dfd55f9188254d39b67a4802a
phev8 <phev8@users.noreply.github.com>
2022-03-09 17:26:12 +0100

    move lyme map header into html

--- src/components/HomeMapComponents/LymeMap.tsx
+++ src/components/HomeMapComponents/LymeMap.tsx
@@ -1,4 +1,4 @@
-import { getExternalOrLocalContentURL, MapWithTimeSliderLoader } from 'case-web-ui';
+import { MapWithTimeSliderLoader } from 'case-web-ui';
 import React from 'react';
 
 interface LymeMapProps {
@@ -8,6 +8,10 @@ const LymeMap: React.FC<LymeMapProps> = (props) => {
   return (
     <div className='p-2 d-flex justify-content-center w-100'>
       <div className='bg-white w-100' style={{ maxWidth: 550 }}>
+        <h5 className='mx-2 mt-2 fw-bold mb-0'>Mensen met erythema migrans</h5>
+        <p className='mx-2 mb-0'>
+          een vroege uiting van de ziekte van Lyme
+        </p>
         <MapWithTimeSliderLoader
           mapUrl={'/data/maps/gem-map-nl.json'}
           dataUrl={'/data/maps/lyme-map-data.json'}
