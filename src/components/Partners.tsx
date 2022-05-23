eab26abc9b935e372e5882c3be3b672913367826
AnMu <92784573+AnMu27011@users.noreply.github.com>
2022-05-23 11:52:48 +0200

    stay in same tap after clicking the partner logos on home page instead of opening a new tab

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -14,7 +14,7 @@ const Partners: React.FC<PartnersProps> = (props) => {
       <div className="d-flex justify-content-center align-items-center flex-wrap">
         <a
           className={imageClassName}
-          href="/informatie/tekenradar" target="_blank" rel="noreferrer">
+          href="/informatie/tekenradar" rel="noreferrer">
           <img src={getExternalOrLocalContentURL("/images/logo-rivm.jpeg")} alt="Logo Rijksinstituut voor Volksgezondheid en Milieu (RIVM)"
             width={280}
             className='mw-100'
@@ -22,7 +22,7 @@ const Partners: React.FC<PartnersProps> = (props) => {
         </a>
         <a
           className={imageClassName}
-          href="/informatie/tekenradar" target="_blank" rel="noreferrer">
+          href="/informatie/tekenradar" rel="noreferrer">
           <img src={getExternalOrLocalContentURL("/images/logo-WUR.png")} alt="Logo Wageningen University and Research"
             width={240}
             className='mw-100'
@@ -30,7 +30,7 @@ const Partners: React.FC<PartnersProps> = (props) => {
         </a>
         <a
           className={imageClassName}
-          href="/informatie/tekenradar" target="_blank" rel="noreferrer">
+          href="/informatie/tekenradar" rel="noreferrer">
           <img src={getExternalOrLocalContentURL("/images/logo-AMC.png")} alt="Logo Amsterdam Universitair Medische Centra"
             width={220}
             className='mw-100'
@@ -38,7 +38,7 @@ const Partners: React.FC<PartnersProps> = (props) => {
         </a>
         <a
           className={imageClassName}
-          href="/informatie/tekenradar" target="_blank" rel="noreferrer">
+          href="/informatie/tekenradar" rel="noreferrer">
           <img src={getExternalOrLocalContentURL("/images/logo-FSD.png")} alt="Logo Stichting voor Duurzame Ontwikkeling"
             width={180}
             className='mw-100'
@@ -46,7 +46,7 @@ const Partners: React.FC<PartnersProps> = (props) => {
         </a>
         <a
           className={imageClassName}
-          href="/informatie/tekenradar" target="_blank" rel="noreferrer">
+          href="/informatie/tekenradar" rel="noreferrer">
           <img src={getExternalOrLocalContentURL("/images/logo-radboudumc.png")} alt="Logo Radboud Universitair Medisch Centrum"
             width={220}
             className='mw-100'
