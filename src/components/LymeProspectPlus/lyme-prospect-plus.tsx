dbb98cc9fa737658d000cfcebeb77057988d37f6
phev8 <phev8@users.noreply.github.com>
2024-08-05 16:09:00 +0200

    fetch lpp participant state

--- src/components/LymeProspectPlus/lyme-prospect-plus.tsx
+++ src/components/LymeProspectPlus/lyme-prospect-plus.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { GenericPageItemProps } from '../utils';
 import { coreReduxActions, PreventAccidentalNavigationPrompt, studyAPI } from 'case-web-app-core';
-import { LoadingPlaceholder } from 'case-web-ui';
+import { LoadingPlaceholder, getExternalOrLocalContentURL } from 'case-web-ui';
 import MissingPidError from './missing-pid-error';
 
 
@@ -10,6 +10,9 @@ type PageMode = 'loading' | 'ready' | 'missingOrWrongPid';
 
 interface LymeProspectPlusProps extends GenericPageItemProps {
 }
+
+const lppAPIRootURL = process.env.REACT_APP_LPP_ROOT_URL ? process.env.REACT_APP_LPP_ROOT_URL : '';
+const apiKey = process.env.REACT_APP_CONTENT_SERVICE_API_KEY ? process.env.REACT_APP_CONTENT_SERVICE_API_KEY : '';
 
 const LymeProspectPlus: React.FC<LymeProspectPlusProps> = (props) => {
   const dispatch = useDispatch();
@@ -26,11 +29,33 @@ const LymeProspectPlus: React.FC<LymeProspectPlusProps> = (props) => {
       return;
     }
 
+    const abortController = new AbortController();
+
+    fetch(getExternalOrLocalContentURL(`${lppAPIRootURL}/lpp/${pid}`), {
+      headers: {
+        'Api-Key': apiKey,
+      },
+      signal: abortController.signal
+    })
+      .then(res => {
+        if (res.status !== 200) {
+          throw new Error('Error fetching LPP data');
+        }
+        return res.json()
+      })
+      .then(json => {
+        console.log(json);
+        setPageMode('ready')
+      })
+      .catch(error => {
+        console.error(error)
+        setPageMode('missingOrWrongPid')
+      });
 
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     dispatch(coreReduxActions.appActions.openSurveyMode(undefined));
     return () => {
+      abortController.abort();
       dispatch(coreReduxActions.appActions.closeSurveyMode());
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
