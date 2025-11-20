dfed733bb24eaded719cd3199d57f3032916c4d6
phev8 <phev8@users.noreply.github.com>
2025-11-20 15:22:48 +0100

    Enhance NotFound page with cover image and additional information
    - Added a cover image to the NotFound page for improved visual appeal.
    - Introduced an EmbeddedMarkdownRenderer to display additional information in the NotFound page.
    - Updated translations for the NotFound page to provide clearer messaging and guidance for users.
    - Refactored ErrorInfo component to accept a className prop for better styling flexibility.

--- app/[locale]/(default)/not-found.tsx
+++ app/[locale]/(default)/not-found.tsx
@@ -1,8 +1,9 @@
 import Container from '@/components/container';
-import ErrorInfo from '@/components/error-info';
+import CoverImage, { CoverImagePosition } from '@/components/cover-image';
+import EmbeddedMarkdownRenderer from '@/components/embedded-markdown-renderer';
 import PageTitlebar from '@/components/page-titlebar';
 import { useTranslations } from 'next-intl';
-
+import ErrorInfo from '@/components/error-info';
 
 export default function NotFound() {
     const t = useTranslations('NotFoundPage');
@@ -10,12 +11,26 @@ export default function NotFound() {
         <PageTitlebar>
             {t('title')}
         </PageTitlebar>
+        <CoverImage
+            src="/static/images/waar-leven-teken.jpg"
+            alt="404"
+            width={1000}
+            height={300}
+            coverImageYPosition="30%"
+        />
 
-        <Container className="py-6 flex flex-col gap-6 md:flex-row">
-            <ErrorInfo
-                title={t('error.title')}
-                description={t('error.description')}
-            />
+        <Container className="py-6 space-y-6 max-w-[600px] mx-auto mb-16">
+            <div className='p-4 border border-destructive rounded-md'>
+                <ErrorInfo
+                    title={t('error.title')}
+                    description={t('error.description')}
+                    className='m-0'
+                />
+            </div>
+
+            <EmbeddedMarkdownRenderer>
+                {t('infos')}
+            </EmbeddedMarkdownRenderer>
         </Container>
     </div>
 }
