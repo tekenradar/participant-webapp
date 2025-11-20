7c062f25a51bae226263e67d6f872cd2a6005926
phev8 <phev8@users.noreply.github.com>
2025-11-20 16:18:29 +0100

    Revert "Add news detail page and page renderer component"
    This reverts commit 42f32037268a07e482b969719f14d1d27af88245.

--- app/[locale]/(default)/nieuws/[...slug]/page.tsx
+++ app/[locale]/(default)/nieuws/[...slug]/page.tsx
@@ -1,3 +1,4 @@
+import { locales } from "@/i18n/routing";
 import { newsPages } from "@/.velite";
 import PageRenderer, { getContent } from "./_components/page-renderer";
 import { setRequestLocale } from "next-intl/server";
@@ -5,23 +6,26 @@ import { setRequestLocale } from "next-intl/server";
 interface PageProps {
     params: Promise<{
         locale: string;
-        newsId: string;
+        slug: string[];
     }>;
 }
 
 export function generateStaticParams() {
-    const staticParams = newsPages.map(({ slugAsParams, locale }) => {
-        return {
-            locale,
-            newsId: slugAsParams.split('/')[0]
-        };
+    const staticParams: Array<{
+        locale: string;
+        slug: string[];
+    }> = [];
+    locales.forEach(locale => {
+        newsPages.forEach(({ slugAsParams }) => {
+            staticParams.push({ locale, slug: slugAsParams.split('/') });
+        });
     });
     return staticParams;
 }
 
 export const generateMetadata = async (props: PageProps) => {
-    const { locale, newsId } = await props.params;
-    const name = newsId;
+    const { locale, slug } = await props.params;
+    const name = slug.join("/");
     const page = await getContent(locale, name);
 
     if (!page) {
@@ -36,10 +40,10 @@ export const generateMetadata = async (props: PageProps) => {
 export default async function Page(
     props: PageProps
 ) {
-    const { locale, newsId } = await props.params;
+    const { locale, slug } = await props.params;
     setRequestLocale(locale);
 
-    const name = newsId;
+    const name = slug.join("/");
     return (
         <PageRenderer
             locale={locale}
