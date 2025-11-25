50f70b0c61b3f7bfb70c1b66a2f709ffa7bc5d66
phev8 <phev8@users.noreply.github.com>
2025-11-25 12:04:05 +0100

    Add research cards section to Onderzoek page with dynamic translations
    - Introduced a new section displaying research cards for various studies on the Onderzoek page, enhancing user engagement.
    - Integrated ImageLinkCard components for each research study, providing titles, descriptions, and images with dynamic translations.
    - Updated Dutch translations to include titles, descriptions, and image alt texts for the new research cards, improving accessibility and content clarity.
    - Added new images to the static assets for use in the research cards.

--- app/[locale]/(default)/onderzoek/page.tsx
+++ app/[locale]/(default)/onderzoek/page.tsx
@@ -4,6 +4,7 @@ import EmbeddedMarkdownRenderer from "@/components/embedded-markdown-renderer";
 import { H2 } from "@/components/headings";
 import PageTitlebar from "@/components/page-titlebar";
 import { getTranslations, setRequestLocale } from "next-intl/server";
+import { ImageLinkCard } from "@/components/image-link-card";
 
 
 interface PageProps {
@@ -56,7 +57,69 @@ export default async function Page(
                 </section>
 
                 <section>
-                    Onze onderzoeken
+                    <Container>
+                        <H2>{t('onderzoeken.title')}</H2>
+                        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-4">
+                            <li className="col-span-1 md:col-span-2">
+                                <ImageLinkCard
+                                    title={t('onderzoeken.cards.vragenlijst.title')}
+                                    moreBtnLabel={t('onderzoeken.readMoreBtn')}
+                                    href="/onderzoek/vragenlijst"
+                                    imageSrc="/static/images/small-pexels-rodnae-productions-10376214y_crop.jpg"
+                                    imageAlt={t('onderzoeken.cards.vragenlijst.imageAlt')}
+                                >
+                                    {t('onderzoeken.cards.vragenlijst.description')}
+                                </ImageLinkCard>
+                            </li>
+                            <li>
+                                <ImageLinkCard
+                                    title={t('onderzoeken.cards.lymeProspect.title')}
+                                    moreBtnLabel={t('onderzoeken.readMoreBtn')}
+                                    href="/onderzoek/lymeProspect"
+                                    imageSrc="/static/images/cRIVM/82073-1920.jpg"
+                                    imageAlt={t('onderzoeken.cards.lymeProspect.imageAlt')}
+                                    imageCredits="© RIVM"
+                                >
+                                    {t('onderzoeken.cards.lymeProspect.description')}
+                                </ImageLinkCard>
+                            </li>
+                            <li>
+                                <ImageLinkCard
+                                    title={t('onderzoeken.cards.preventieveAntibiotica.title')}
+                                    moreBtnLabel={t('onderzoeken.readMoreBtn')}
+                                    href="/onderzoek/preventieveAntibiotica"
+                                    imageSrc="/static/images/cRIVM/iStock-159286807-1920.jpg"
+                                    imageAlt={t('onderzoeken.cards.preventieveAntibiotica.imageAlt')}
+                                    imageCredits="© RIVM"
+                                >
+                                    {t('onderzoeken.cards.preventieveAntibiotica.description')}
+                                </ImageLinkCard>
+                            </li>
+                            <li>
+                                <ImageLinkCard
+                                    title={t('onderzoeken.cards.victory.title')}
+                                    moreBtnLabel={t('onderzoeken.readMoreBtn')}
+                                    href="/onderzoek/victory"
+                                    imageSrc="/static/images/pexels-katerina-holmes-5905747.jpg"
+                                    imageAlt={t('onderzoeken.cards.victory.imageAlt')}
+                                >
+                                    {t('onderzoeken.cards.victory.description')}
+                                </ImageLinkCard>
+                            </li>
+                            <li>
+                                <ImageLinkCard
+                                    title={t('onderzoeken.cards.pandora.title')}
+                                    moreBtnLabel={t('onderzoeken.readMoreBtn')}
+                                    href="/onderzoek/pandora"
+                                    imageSrc="/static/images/cRIVM/JPJDouaneLaboratorium17-1920.jpg"
+                                    imageAlt={t('onderzoeken.cards.pandora.imageAlt')}
+                                    imageCredits="© RIVM"
+                                >
+                                    {t('onderzoeken.cards.pandora.description')}
+                                </ImageLinkCard>
+                            </li>
+                        </ul>
+                    </Container>
                 </section>
 
                 <section>
