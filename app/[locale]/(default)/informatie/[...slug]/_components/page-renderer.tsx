7eff8caf5dfac9fe8e4e80d22328a50c73da9dc5
phev8 <phev8@users.noreply.github.com>
2025-11-26 10:13:31 +0100

    Add navigation for related pages in info articles
    - Enhanced the `velite.config.ts` to include optional fields for previous and next page URLs, titles, and descriptions, as well as an overview page key.
    - Updated the `PageRenderer` component to conditionally render related pages based on the new fields.
    - Added `overviewPageKey` to several partner MDX files to link to the overview page.
    - Expanded Dutch translations for related pages, including titles, descriptions, and button labels for better user navigation.

--- app/[locale]/(default)/informatie/[...slug]/_components/page-renderer.tsx
+++ app/[locale]/(default)/informatie/[...slug]/_components/page-renderer.tsx
@@ -7,6 +7,7 @@ import CoverImage, { CoverImagePosition } from '@/components/cover-image';
 import IndexPageLayout from '@/components/layouts/index-page-layout';
 import { getTranslations } from 'next-intl/server';
 import ReportCard from '@/components/report-card';
+import RelatedPages from '@/components/related-pages';
 
 
 export const getContent = async (locale: string, name: string) => {
@@ -59,6 +60,12 @@ const PageRenderer: React.FC<InfopageRendererProps> = async (props) => {
             );
         case 'article':
         default:
+
+            const hasPreviousPage = pageContent.previousPageURL && pageContent.previousPageTitle;
+            const hasNextPage = pageContent.nextPageURL && pageContent.nextPageTitle;
+
+            const hasOverviewPage = pageContent.overviewPageKey;
+
             return (
                 <ArticlePageLayout
                     title={<>
@@ -77,6 +84,38 @@ const PageRenderer: React.FC<InfopageRendererProps> = async (props) => {
                     sideBarContent={<div className='flex justify-center'>
                         <ReportCard showMyTekenradarLink={false} />
                     </div>}
+                    bottomContent={<div
+                        className={'mt-6 grow lg:max-w-[728px] w-full'}
+                    >
+                        <RelatedPages
+                            title={t('relatedPages.title')}
+                            previousPage={hasPreviousPage ? {
+                                key: 'previousPage',
+                                href: pageContent.previousPageURL,
+                                title: pageContent.previousPageTitle,
+                                description: pageContent.previousPageDescription,
+                                btnLabel: t('relatedPages.readMoreBtn'),
+                                imageURL: '/static/images/waar-leven-teken.jpg',
+                            } : undefined}
+                            nextPage={hasNextPage ? {
+                                key: 'nextPage',
+                                href: pageContent.nextPageURL,
+                                title: pageContent.nextPageTitle,
+                                description: pageContent.nextPageDescription,
+                                btnLabel: t('relatedPages.readMoreBtn'),
+                            } : undefined}
+                            overviewPage={hasOverviewPage ? {
+                                key: 'overviewPage',
+                                href: `/informatie/${pageContent.overviewPageKey}`,
+                                title: t(`relatedPages.overviewPageReferences.${pageContent.overviewPageKey}.title`),
+                                description: t(`relatedPages.overviewPageReferences.${pageContent.overviewPageKey}.description`),
+                                btnLabel: t(`relatedPages.overviewPageReferences.${pageContent.overviewPageKey}.btnLabel`),
+                                imageURL: '/static/images/waar-leven-teken.jpg',
+                                imageMode: 'left',
+                            } : undefined}
+                        />
+                    </div>
+                    }
                 >
                     <MDXContent
                         code={pageContent.content}
