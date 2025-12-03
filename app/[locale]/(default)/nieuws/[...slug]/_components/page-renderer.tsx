b60e51ef67713b6ec4b69b79daed171c13520b65
phev8 <phev8@users.noreply.github.com>
2025-12-03 09:50:12 +0100

    Add date display for related pages in PageRenderer component and update related page interface. Introduce new news article on tick bites and add corresponding images for enhanced content presentation.

--- app/[locale]/(default)/nieuws/[...slug]/_components/page-renderer.tsx
+++ app/[locale]/(default)/nieuws/[...slug]/_components/page-renderer.tsx
@@ -83,6 +83,7 @@ const PageRenderer: React.FC<InfopageRendererProps> = async (props) => {
                         title: previousPage.title,
                         description: previousPage.teaserText,
                         btnLabel: t('relatedPages.readMoreBtn'),
+                        date: new Date(previousPage.date).toLocaleDateString('nl-NL', { year: 'numeric', month: 'long', day: 'numeric' }),
                         // imageURL: previousPage.cover,
                     } : undefined}
                     nextPage={nextPage ? {
@@ -90,6 +91,7 @@ const PageRenderer: React.FC<InfopageRendererProps> = async (props) => {
                         title: nextPage.title,
                         description: nextPage.teaserText,
                         btnLabel: t('relatedPages.readMoreBtn'),
+                        date: new Date(nextPage.date).toLocaleDateString('nl-NL', { year: 'numeric', month: 'long', day: 'numeric' }),
                         // imageURL: nextPage.cover,
                     } : undefined}
                     overviewPage={{
