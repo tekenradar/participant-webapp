1b6545db862bb62c67eb62ba26a58cb772fa5925
phev8 <phev8@users.noreply.github.com>
2025-12-03 16:09:35 +0100

    Update LatestNewsSection to accept locale prop and enhance news display with dynamic content. Add translations for new buttons and descriptions in Dutch.

--- app/[locale]/(default)/_components/latest-news-section.tsx
+++ app/[locale]/(default)/_components/latest-news-section.tsx
@@ -1,11 +1,18 @@
 import Container from "@/components/container"
 import { H2 } from "@/components/headings";
+import { ImageLinkCard } from "@/components/image-link-card";
+import { getSortedNewsArticles } from "@/lib/utils/news-items-utils";
 import { getTranslations } from "next-intl/server";
 
-const LatestNewsSection = async () => {
+const draftMode = process.env.NEXT_DRAFT_MODE === 'true';
+
+const LatestNewsSection = async (props: { locale: string }) => {
     const t = await getTranslations('LandingPage.latestNews');
 
+    const sortedNewsItems = await getSortedNewsArticles(props.locale, draftMode);
 
+
+    const topNewsItem = sortedNewsItems[0];
     return (
         <section>
             <Container>
@@ -15,9 +22,47 @@ const LatestNewsSection = async () => {
                 >
                     {t('title')}
                 </H2>
-                <div className="h-80 flex bg-muted justify-center items-center w-full border border-border rounded-md border-dashed mt-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-4">
+                    <ImageLinkCard
+                        title={topNewsItem.title}
+                        moreBtnLabel={t('readMoreBtn')}
+                        dateLabel={new Date(topNewsItem.date).toLocaleDateString(props.locale, { year: 'numeric', month: 'long', day: 'numeric' })}
+                        imagePlacement="top"
+                        imageHeightClassName="max-h-[250px] min-h-[250px]"
+                        href={`${topNewsItem.slug}`}
+                        imageSrc={topNewsItem.teaserImage?.src}
+                        imageAlt={topNewsItem.title}
+                    >
+                    </ImageLinkCard>
 
-                    todo: top news
+                    <ul className="space-y-4">
+                        {sortedNewsItems.slice(1, 3).map((newsItem) => (
+                            <li key={newsItem.slug}>
+                                <ImageLinkCard
+                                    title={newsItem.title}
+                                    moreBtnLabel={t('readMoreBtn')}
+                                    dateLabel={new Date(newsItem.date).toLocaleDateString(props.locale, { year: 'numeric', month: 'long', day: 'numeric' })}
+                                    href={`${newsItem.slug}`}
+                                    imagePlacement="left"
+                                    imageSrc={newsItem.teaserImage?.src}
+                                    imageAlt={newsItem.title}
+                                >
+
+                                </ImageLinkCard>
+                            </li>
+                        ))}
+
+                        <li>
+                            <ImageLinkCard
+                                title={t('goToNewsPage.title')}
+                                moreBtnLabel={t('goToNewsPage.btnLabel')}
+                                href="/nieuws"
+                            >
+                                {t('goToNewsPage.description')}
+                            </ImageLinkCard>
+
+                        </li>
+                    </ul>
                 </div>
             </Container>
         </section>
