0b87b4b5d5d7148f0279cafc525a9f11c427dcf7
phev8 <phev8@users.noreply.github.com>
2025-12-03 09:30:57 +0100

    Refactor news article retrieval in page-renderer component to use sorted news items and implement related pages feature. Add new news articles and images, and update translations for improved user experience.

---
+++ lib/utils/news-items-utils.ts
@@ -0,0 +1,21 @@
+import { NewsItem, newsPages } from "@/.velite";
+
+export const getSortedNewsArticles = async (locale: string, draftMode: boolean) => {
+    const localeNews = filterNewsItemsForLocale(newsPages, locale, draftMode);
+    return sortNewsItems(localeNews);
+}
+
+export const filterNewsItemsForLocale = (newsItems: Array<NewsItem>, locale: string, includeDrafts: boolean) => {
+    return newsItems.filter(newsItem => {
+        const published = new Date(newsItem.date) < new Date();
+        return newsItem.locale === locale && (published || includeDrafts)
+    });
+}
+
+export function sortNewsItems(newsItem: Array<NewsItem>) {
+    return newsItem.sort((a, b) => {
+        if (a.date > b.date) return -1;
+        if (a.date < b.date) return 1;
+        return 0;
+    });
+}
