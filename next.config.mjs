442b5eb8367b0a3cfb0d0a4ed8bd32c5ab29f39f
phev8 <phev8@users.noreply.github.com>
2025-12-10 12:10:55 +0100

    Update Next.js configuration and dependencies: refactor VELITE_STARTED logic in next.config.mjs, upgrade velite to version 0.3.1, and update React and React DOM versions in package.json for improved compatibility and performance.

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const isDev = process.argv.indexOf('dev') !== -1
 const isBuild = process.argv.indexOf('build') !== -1
 if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
-    process.env.VELITE_STARTED = '1'
-    const { build } = await import('velite')
-    await build({ watch: isDev, clean: !isDev })
+  process.env.VELITE_STARTED = '1'
+  const { build } = await import('velite')
+  await build({ watch: isDev, clean: !isDev })
 }
 
 const withNextIntl = createNextIntlPlugin();
