b65759d19be64fa90013df8b3ad01d8ecdc6abc9
phev8 <phev8@users.noreply.github.com>
2025-12-15 22:06:08 +0100

    Enhance SurveySkeleton component with optional title visibility
    - Added a `hideTitle` prop to the `SurveySkeleton` component to conditionally render the title bar.
    - Improved class name handling for the main container based on the `hideTitle` prop, ensuring better layout control.

--- components/survey-components/survey-skeleton.tsx
+++ components/survey-components/survey-skeleton.tsx
@@ -1,14 +1,21 @@
 import PageTitlebar from "@/components/page-titlebar";
 import SurveyItemSkeleton from "@/components/survey-components/survey-item-skeleton";
 import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
 
-const SurveySkeleton = () => {
+
+interface SurveySkeletonProps {
+    hideTitle?: boolean;
+}
+
+const SurveySkeleton: React.FC<SurveySkeletonProps> = ({ hideTitle = false }) => {
     return (
         <div>
-            <PageTitlebar>
+            {!hideTitle && (<PageTitlebar>
                 <Skeleton className="w-64 h-8 bg-primary/20" />
             </PageTitlebar>
-            <div className="max-w-[800px] mx-auto pt-6 pb-10 survey @container">
+            )}
+            <div className={cn("max-w-[800px] mx-auto pt-6 pb-10 survey @container", hideTitle ? 'pt-0' : 'pt-6')}>
                 <div className="space-y-4">
                     <SurveyItemSkeleton variant="display"
                         showBottomText={true}
