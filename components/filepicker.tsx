dedd6047735996ca309c33655904c21dad68c020
phev8 <phev8@users.noreply.github.com>
2025-12-19 13:52:47 +0100

    Add Filepicker component for file uploads
    - Introduced a new `Filepicker` component utilizing `react-dropzone` for file selection.
    - Implemented drag-and-drop functionality with customizable placeholders and labels.
    - Enhanced user experience by displaying selected file names and providing visual feedback during file selection.

---
+++ components/filepicker.tsx
@@ -0,0 +1,85 @@
+import clsx from 'clsx';
+import { FileUpIcon } from 'lucide-react';
+import React, { useEffect } from 'react';
+import { Accept, FileWithPath, useDropzone } from "react-dropzone";
+
+
+interface FilepickerProps {
+    id: string;
+    label?: string;
+    accept?: Accept;
+    disabled?: boolean;
+
+    placeholders?: {
+        upload: string;
+        drag: string;
+    }
+
+    onChange: (files: readonly FileWithPath[]) => void;
+}
+
+const Filepicker: React.FC<FilepickerProps> = (props) => {
+    const { onChange } = props;
+    const {
+        acceptedFiles,
+        isDragActive,
+        getRootProps, getInputProps } = useDropzone({
+            accept: props.accept,
+            disabled: props.disabled,
+        });
+
+
+    useEffect(() => {
+        onChange(acceptedFiles);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [acceptedFiles]);
+
+    const placeholders = {
+        upload: props.placeholders?.upload || 'Click to pick a file',
+        drag: props.placeholders?.drag || 'or Drag and drop'
+    }
+
+    return (
+        <div className=''>
+            {props.label && (
+                <label
+                    htmlFor={props.id}
+                    className="block text-sm font-medium text-foreground pb-1.5"
+                >
+                    {props.label}
+                </label>
+            )}
+
+            <div
+                className={clsx("border-2 border-dashed border-neutral-200  rounded-md p-4 hover:border-neutral-400 cursor-pointer outline-black",
+                    {
+                        "border-neutral-400 bg-neutral-100": isDragActive,
+                    }
+                )}
+                {...getRootProps()}>
+                <input
+                    id={props.id}
+                    disabled={props.disabled}
+                    className=""
+                    {...getInputProps()} />
+                <div className='flex justify-center items-center gap-1'>
+                    {acceptedFiles.length > 0 ? <p>{
+                        acceptedFiles[0].name
+                    }</p> : <>
+                        <FileUpIcon className='text-2xl text-neutral-400' />
+                        <p>
+                            <span className='text-cyan-800 me-1'>
+                                {placeholders.upload}
+                            </span>
+                            <span>
+                                {placeholders.drag}
+                            </span>
+                        </p>
+                    </>}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Filepicker;
