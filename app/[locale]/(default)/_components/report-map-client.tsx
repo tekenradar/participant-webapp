cbd8c5a88c2b305299d3b2710f34bb672b7cff81
phev8 <phev8@users.noreply.github.com>
2025-12-24 10:19:15 +0100

    Add ReportMap component and error handling in Dutch translations
    - Integrated the `ReportMap` component into the `InfoTabs` for displaying tick reports.
    - Enhanced the Dutch translations by adding error messages for loading tick reports, improving user feedback during failures.

---
+++ app/[locale]/(default)/_components/report-map-client.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from 'react';
+
+const ReportMapClient = () => {
+    return <div>ReportMapClient</div>;
+};
+
+export default ReportMapClient;
---
+++ app/[locale]/(default)/_components/report-map.tsx
@@ -0,0 +1,57 @@
+import { Suspense } from "react";
+import ReportMapClient from "./report-map-client";
+import SimpleLoader from "@/components/simple-loader";
+import { getTranslations } from "next-intl/server";
+import ErrorInfo from "@/components/error-info";
+
+
+const ReportMapLoader = async () => {
+    const t = await getTranslations('LandingPage.infoTabs.tekenmeldingen');
+
+    const dataURL = process.env.TB_MAP_DATA_URL;
+    const apiKey = process.env.CONTENT_SERVICE_API_KEY;
+
+    if (!dataURL || !apiKey) {
+        return <div>Error: TB_MAP_DATA_URL or CONTENT_SERVICE_API_KEY is not set</div>;
+    }
+
+    const response = await fetch(`${dataURL}`, {
+        headers: {
+            'Api-Key': apiKey
+        }
+    });
+
+    if (!response.ok) {
+        let errorMessage = response.statusText;
+        try {
+            const errorBody = await response.json();
+            errorMessage = errorBody.error || response.statusText;
+        } catch {
+            // If response is not JSON, use statusText
+        }
+        return <div className="w-full h-full flex justify-center items-center p-4 sm:p-6">
+            <ErrorInfo
+                title={t('error.title')}
+                description={t('error.description', { errorMessage })}
+            />
+        </div>
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+
+    return <ReportMapClient />;
+};
+
+const ReportMap = async () => {
+    return <Suspense fallback={<ReportMapSkeleton />}>
+        <ReportMapLoader />
+    </Suspense>;
+};
+
+const ReportMapSkeleton = () => {
+    return <SimpleLoader className="w-full h-full" />;
+};
+
+export default ReportMap;
